eb82b4a659dad9a6f26dba1fefa29b207b5e1f45
odusdegreat <tobiodubote@gmail.com>
2025-04-27 23:24:02 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import k from "./kaplayCtx";
+
+k.loadSprite("chemical1-bg", "graphics/chemical1-bg.jpg");
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+  sliceX: 8,
+  sliceY: 2,
+  anims: {
+    run: { from: 0, to: 7, loop: true, speed: 30 },
+    jump: { from: 8, to: 15, loop: true, speed: 100 },
+  },
+});
+k.loadSprite("rings", "graphics/rings.png", {
+  sliceX: 16,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 15, loop: true, speed: 30 },
+  },
+});
+k.loadSprite("motobug", "graphics/motobug.png", {
+  sliceX: 5,
+  sliceY: 1,
+  anims: {
+    run: { from: 0, to: 4, loop: true, speed: 8 },
+  },
+});
+k.loadFont("mania", "fonts/mania.ttf");
